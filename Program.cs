2e15d454675f21c25224735933a4e393c2eca84e
Gbohunmi Francis <120693899+Gbohunmifrancis@users.noreply.github.com>
2024-06-10 22:15:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
